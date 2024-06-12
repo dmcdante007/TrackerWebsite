08d38345136afdc90488508e834c38a9040bff8a
dmcdante007 <meshramswaraj98>
2024-06-12 18:02:23 +0530

    "Fetch-for-SignIn-and-SignUp"

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContextApi = React.createContext({
 
@@ -7,14 +7,35 @@ const ContextApi = React.createContext({
 
 
 export const ContextProvider = (props)=> {
+    const initialToken = localStorage.getItem('token')
+    const [token, setToken] = useState(initialToken);
+    const userIsLoggedIn = !!token;
 
+    const loginHandler = (tok)=> {
+        setToken(tok);
+        localStorage.setItem('token', tok);
+
+        setTimeout(()=>{
+            logouthandler();
+            
+        }, 5000)
+       
+    };
+
+    const logouthandler = ()=> {
+        setToken(null)
+        localStorage.removeItem('token')
+    };
 
 
     const init = {
-
+            token: token,
+            isLoggedIn: userIsLoggedIn,
+            login: loginHandler,
+            logout: logouthandler,
 
     }
-    return <ContextApi.Provider value={{init}}>
+    return <ContextApi.Provider value={init}>
         {props.children}
     </ContextApi.Provider>
 }
--- src/components/Pages/SignUpPage.jsx
+++ src/components/Pages/SignUpPage.jsx
@@ -1,26 +1,67 @@
-import { Container } from "@mui/material";
-import React, { useRef, useState } from "react";
-import load from '../assets/loadingNor.gif'
+import React, { useRef, useState, useContext } from "react";
+import load from "../assets/loadingNor.gif";
+import ContextApi from "../Context";
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
-    const [isLogin ,setIsLogin] = useState(false);
-    const emailInputRef = useRef();
-    const passwordInputRef = useRef();
-    const [isLoading, setIsloding] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const [isLoading, setIsloding] = useState(false);
+    const ctx = useContext(ContextApi)
 
-
-
-
-    const submitHandler = (e)=>{
-        e.preventDefault();
-
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setIsloding(true);
+    const emailIs = emailInputRef.current.value;
+    const passIs = passwordInputRef.current.value;
+    let url;
+    if(isLogin){
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAvOmNGLPl3_8el7x7ah_Ha8s9rF9P5pb4'
+    }
+    else{
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAvOmNGLPl3_8el7x7ah_Ha8s9rF9P5pb4'
     }
 
-    const switchAuthModeHandler = ()=> {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            email : emailIs,
+            password : passIs,
+            returnSecureToken: true,
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+    .then((res)=>{
+        console.log('in 1st Then')
+        setIsloding(false);
+        if(res.ok){
+            return res.json()
+        }
+        else{
+            return res.json().then((data) => {
+              let errorMessage = "Authentication failed!";
+              throw new Error(errorMessage);
+            });
+          }
 
-        setIsLogin(!isLogin);
+    }).then((data) => {
+        console.log('In 2nd Then')
+        ctx.login(data.idToken);
+        // history.replace('/');
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
 
-    }
+  };
+
+  const switchAuthModeHandler = () => {
+    setIsLogin(!isLogin);
+  };
 
   return (
     <>
@@ -42,19 +83,16 @@ const SignupPage = () => {
           </div>
           <div>
             {!isLoading && (
-              <button type="submit">
-                {isLogin ? "Login" : "Sign up"}
-              </button>
+              <button type="submit">{isLogin ? "Login" : "Sign up"}</button>
             )}
             {isLoading && (
               <img style={{ height: "20px" }} src={load} alt="help"></img>
             )}
-            <br/>
-            <button
-              type="button"
-              onClick={switchAuthModeHandler}
-            >
-              {isLogin ? "Dont have and Account? Create new account" : "Already have an Account? Login"}
+            <br />
+            <button type="button" onClick={switchAuthModeHandler}>
+              {isLogin
+                ? "Dont have and Account? Create new account"
+                : "Already have an Account? Login"}
             </button>
           </div>
         </form>
