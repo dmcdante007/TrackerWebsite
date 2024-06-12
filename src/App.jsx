15c85f6daaaf4f572e8da77f0e369167c5234dec
dmcdante007 <meshramswaraj98>
2024-06-12 13:44:28 +0530

    "Toggle-between-Login-n-SignUp"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/Pages/Header/Header'
 import SignupPage from './components/Pages/SignUpPage'
+import { Router } from 'react-router-dom'
+
 
 function App() {
 
--- src/components/Pages/SignUpPage.jsx
+++ src/components/Pages/SignUpPage.jsx
@@ -1,57 +1,64 @@
 import { Container } from "@mui/material";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import load from '../assets/loadingNor.gif'
 
 const SignupPage = () => {
+    const [isLogin ,setIsLogin] = useState(false);
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+    const [isLoading, setIsloding] = useState(false);
+
+
+
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+
+    }
+
+    const switchAuthModeHandler = ()=> {
+
+        setIsLogin(!isLogin);
+
+    }
+
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "white",
-
-          width: "250px",
-          height: "300px",
-          textAlign: "center",
-        }}
-      >
-        <form
-          style={{
-            marginTop: "250px",
-            marginLeft: "700px",
-            width: "250px",
-            height: "300px",
-            textAlign: "center",
-            border: "2px soild #04AA6D",
-          }}
-        >
-          <h2 style={{}}>Sign Up</h2>
-          <input id="email" type="text" placeholder="Email" required />
-          <br />
-          <input
-            id="password"
-            type="password"
-            placeholder="password"
-            required
-          />{" "}
-          <br />
-          <input
-            type="password"
-            id="confirmPass"
-            placeholder="Confirm Password"
-          />{" "}
-          <br />
-          <button
-            type="submit"
-            style={{
-              width: "70%",
-              height:'30px',
-              borderRadius: "10px",
-              backgroundColor: "blue",
-            }}
-          >
-            Sign up
-          </button>
+      <section>
+        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+        <form onSubmit={submitHandler}>
+          <div>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" ref={emailInputRef} required />
+          </div>
+          <div>
+            <label htmlFor="password">Your Password</label>
+            <input
+              type="password"
+              id="password"
+              ref={passwordInputRef}
+              required
+            />
+          </div>
+          <div>
+            {!isLoading && (
+              <button type="submit">
+                {isLogin ? "Login" : "Sign up"}
+              </button>
+            )}
+            {isLoading && (
+              <img style={{ height: "20px" }} src={load} alt="help"></img>
+            )}
+            <br/>
+            <button
+              type="button"
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? "Dont have and Account? Create new account" : "Already have an Account? Login"}
+            </button>
+          </div>
         </form>
-      </div>
+      </section>
     </>
   );
 };
